5fc131f3cd80bba1e4fbbaa61ea067c04a5843fb
Elton Lucien <61792119+Elton360@users.noreply.github.com>
2022-08-03 15:05:29 -0400

    Styling Added

--- src/pages/authPage/AuthPage.js
+++ src/pages/authPage/AuthPage.js
@@ -7,6 +7,11 @@ function AuthPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    return;
+  };
+
   const toggleHandler = () => {
     setIsLogin(!isLogin);
   };
@@ -14,30 +19,49 @@ function AuthPage() {
   return (
     <div className={classes.main_container}>
       <div className={classes.info}>
-        <h2 className={classes.title}>Tech Reddit</h2>
+        <h2 className={classes.title}>Logo or Title</h2>
+        <p className={classes.description}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus nisi
+          odio praesentium aperiam asperiores fuga. Iure, autem ullam rem nisi
+          perferendis.
+        </p>
       </div>
       <form className={classes.form}>
-        <span className={classes.title}>{isLogin ? "Login" : "Sign Up"}</span>
-        <div className={classes.email_container}>
-          <label className={classes.label}>Email</label>
+        <span className={classes.header}>{isLogin ? "Login" : "Sign Up"}</span>
+        <div className={classes.text_container}>
           <input
-            type="email"
+            className={classes.text_input}
+            type="text"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
+            placeholder="Email or Username"
           />
         </div>
         <div className={classes.email_container}>
-          <label className={classes.label}>Password</label>
           <input
+            className={classes.text_input}
             type="password"
             onChange={(e) => setPassword(e.target.value)}
             value={password}
+            placeholder="Password"
           />
         </div>
+        <button
+          className={`btn ${classes.btn_submit} ${
+            email && password && "btn_valid"
+          }`}
+          onClick={submitHandler}
+        >
+          Submit
+        </button>
+
+        <input
+          className={`btn ${classes.btn_toggle}`}
+          type="button"
+          value={!isLogin ? "Login" : "Sign Up"}
+          onClick={toggleHandler}
+        />
       </form>
-      <button className={classes.toggler} onClick={toggleHandler}>
-        {!isLogin ? "Login" : "Sign Up"}
-      </button>
     </div>
   );
 }
