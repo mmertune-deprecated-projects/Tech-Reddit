df2fe622b37be34b739d0ca076b681fcca24328d
Elton Lucien <61792119+Elton360@users.noreply.github.com>
2022-08-10 12:22:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import AuthPage from "./pages/authPage/AuthPage";
-import HomePage from "./pages/homepage/HomePage";
-import Nav from "./components/Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AuthPage from "./pages/authPage/AuthPage";
+import HomePage from "./pages/homePage/HomePage";
+
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
