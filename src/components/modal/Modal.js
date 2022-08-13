4e241f399d380d57b6b10e0a21154f8a3ef13413
Elton Lucien <61792119+Elton360@users.noreply.github.com>
2022-08-13 12:48:58 -0400

    Multipurpose Modal

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import classes from "./modal.module.css";
+
+//The dark overlay that covers the screen
+//onClick should normally be a function that unmounts Modal
+const BackDrop = (props) => (
+  <div className={classes.backdrop} onClick={props.onClick} />
+);
+
+const Overlay = (props) => (
+  <div className={classes.overlay}>{props.component}</div>
+);
+
+function Modal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop onClick={props.onClick} />,
+        document.getElementById("modal-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay component={props.component} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
+  );
+}
+
+export default Modal;
