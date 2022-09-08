bbfb8819c4e3c2f009225138a49462140262cac2
Marvens Mertune <marvensmertune@gmail.com>
2022-09-08 19:32:51 -0400

    Add x-Button to erase input
    Some modifications were made to the button styles. The x-button was
    added and has the ability to erase input after selecting the button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./Nav.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import Button from "./button/Button";
+import { useRef } from "react";
 
 const Nav = () => {
+  const inputVal = useRef(null);
+
+  const clearInput = () => {
+    inputVal.current.value = "";
+  };
+
   return (
     <div className={classes.nav}>
       <Link to="/home" className={classes.icon}>
@@ -16,19 +23,34 @@ const Nav = () => {
       </Link>
       <form className={classes.search_container}>
         <label htmlFor="search_input" className={classes.search_label}>
-        <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon icon={faSearch} />
         </label>
         <input
           type="text"
           placeholder="Search"
           className={classes.search_input}
           id="search_input"
+          ref={inputVal}
+        />
+        <FontAwesomeIcon
+          onClick={clearInput}
+          icon={faXmark}
+          className={classes.xmark}
         />
       </form>
       <div className={classes.link_container}>
-        <Button title="Login" link="/auth" loginState = {true} classProp = "login_bttn"/>
-        <Button title="Sign Up" link="/auth" loginState = {false} classProp = "signup_bttn"/>
-
+        <Button
+          title="Login"
+          link="/auth"
+          loginState={true}
+          classProp="login_bttn"
+        />
+        <Button
+          title="Sign Up"
+          link="/auth"
+          loginState={false}
+          classProp="signup_bttn"
+        />
       </div>
     </div>
   );
